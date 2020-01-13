7ff15bada0bfa0f93185bdf1acc79cdc0b4cec86
Annika <10848892@my.uvu.edu>
2020-01-13 11:14:54 -0700

    printed a message in unity

---
+++ Project1/Assets/Scripts/NewClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        print("yeet them feet");
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        print("and I oop");
+    }
+}
