84b72a10fc58349889ca6dcf73d9c69000af01b5
Annika <10848892@my.uvu.edu>
2020-01-15 10:50:10 -0700

    learned data types

--- Project1/Assets/Scripts/NewClass.cs
+++ Project1/Assets/Scripts/NewClass.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class NewClass : MonoBehaviour
 {
+
+    public string nameString;
+    public int intNum;
+    public float floatNum;
+    public GameObject gameObj;
+    
     // Start is called before the first frame update
     private void Start()
     {
-        print("yeet them feet");
+        print(nameString);
+        print(intNum);
+        print(floatNum);
+        print(gameObj);
     }
 
     // Update is called once per frame
