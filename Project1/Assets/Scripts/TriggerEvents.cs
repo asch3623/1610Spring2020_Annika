83a76c7f579380b5f36aa30560bb93374ffd2375
Annika <10848892>
2020-01-28 17:37:36 -0700

    worked on triggers

--- Project1/Assets/Scripts/TriggerEvents.cs
+++ Project1/Assets/Scripts/TriggerEvents.cs
@@ -1,12 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class TriggerEvents : MonoBehaviour
 {
     public UnityEvent triggerEnterEvent;
+    public UnityEvent triggerStayEvent;
+    public UnityEvent triggerExitEvent;
     
     private void OnTriggerEnter(Collider other)
     {
        triggerEnterEvent.Invoke();
+       print("cube has touched the sphere");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print("cube is in position");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        print("you left");
     }
 }
