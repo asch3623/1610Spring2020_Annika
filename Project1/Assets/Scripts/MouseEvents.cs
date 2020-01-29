3ac6b42a538704d17b7975f4016fc3df237738f4
Annika <10848892@my.uvu.edu>
2020-01-29 11:19:58 -0700

    learned about mouse events

---
+++ Project1/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseEvents : MonoBehaviour
+{
+
+    public UnityEvent mouseDown;
+    public UnityEvent mouseUp;
+    public UnityEvent mouseEnter;
+    public UnityEvent mouseExit, mouseOver, mouseDrag;
+    
+    private void OnMouseDown()
+    {
+        mouseDown.Invoke();
+    }
+    private void OnMouseUp()
+    {
+        mouseUp.Invoke();
+    }
+    private void OnMouseEnter()
+    {
+        mouseEnter.Invoke();
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOver.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        mouseExit.Invoke();
+    }
+
+    private void OnMouseDrag()
+    {
+        mouseDrag.Invoke();
+    }
+}
---
+++ Project1/Assets/Scripts/TriggeringEvents.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeringEvents : MonoBehaviour
+{
+    public int myInt;
+    public string myString;
+    public float myFloat;
+    public UnityEvent enterEvent;
+    public UnityEvent stayEvent;
+    public UnityEvent exitEvent;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        myInt = 7;
+        print(myInt);
+        enterEvent.Invoke();
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        myFloat = 5;
+        print(myFloat);
+        stayEvent.Invoke();
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        print(myString);
+        exitEvent.Invoke();
+    }
+}
