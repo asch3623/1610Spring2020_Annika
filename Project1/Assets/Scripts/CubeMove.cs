9ec448c6817e8a3c1038b3975f8c2a549ef6687d
Annika <10848892@my.uvu.edu>
2020-02-05 11:33:57 -0700

    learned double jumps

---
+++ Project1/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.UIElements.StyleEnums;
+
+public class CubeMove : MonoBehaviour
+{
+    public CharacterController controller;
+    private Vector3 positionDirection;
+    public float speed = 6f;
+    public float gravity = 3f;
+    public float jumpForce = 30f;
+    public int jumpCountMax = 2;
+    public static double jumpCount;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        controller.Move(positionDirection * Time.deltaTime);
+        positionDirection.x = Input.GetAxis("Vertical")*speed;
+        positionDirection.z = Input.GetAxis("Horizontal")*-speed;
+        positionDirection.y -= gravity;
+
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            positionDirection.y = 0;
+        }
+        
+        //double jump
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            positionDirection.y = jumpForce;
+            jumpCount++;
+        }
+    }
+}
