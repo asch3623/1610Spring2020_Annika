8e2f2e06b06ca5d447371e35d4fceb85ac692e3b
Annika <10848892>
2020-02-15 14:02:55 -0700

    Random Numbers

---
+++ Project1/Assets/Scripts/RandomRangeScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomRangeScript : MonoBehaviour
+{
+    private int speed;
+
+    private Vector3 direction;
+    // tutorial from HelloWorld Studios
+    void Start()
+    {
+        speed = Random.Range(0,7);
+        print(speed);
+        direction = new Vector3(Random.Range(-3, 3), 0, Random.Range(-3, 3));
+    }
+
+    private void Update()
+    {
+        transform.Translate( direction * Time.deltaTime * speed);
+    }
+
+}
