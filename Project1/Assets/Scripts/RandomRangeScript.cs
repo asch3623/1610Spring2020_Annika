7710f984b55fe72eb3102c9d7284588b6a3176c6
Annika <10848892>
2020-02-15 14:35:39 -0700

    While loop

--- Project1/Assets/Scripts/RandomRangeScript.cs
+++ Project1/Assets/Scripts/RandomRangeScript.cs
@@ -4,11 +4,13 @@ using Random = UnityEngine.Random;
 public class RandomRangeScript : MonoBehaviour
 {
     private int speed;
+    private int i = 1;
 
     private Vector3 direction;
-    // tutorial from HelloWorld Studios
+    // random number tutorial from HelloWorld Studios
     void Start()
     {
+       
         speed = Random.Range(0,7);
         print(speed);
         direction = new Vector3(Random.Range(-3, 3), 0, Random.Range(-3, 3));
@@ -16,7 +18,16 @@ public class RandomRangeScript : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate( direction * Time.deltaTime * speed);
+        //while loops learned from Charger Games youtube video
+         while (i <= 6)
+                {
+                    transform.Translate( direction * Time.deltaTime * speed);
+                    print("the value of i is" + i);
+                    i++;
+                }
+
+         print("stopped");
+         transform.Translate(0, 0, 0);
     }
 
 }
