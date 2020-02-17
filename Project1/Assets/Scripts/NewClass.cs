86c7bda730579f06b077a07914897642a29a6fd9
Annika <10848892@my.uvu.edu>
2020-02-16 21:50:00 -0700

    Destroyed An Object

--- Project1/Assets/Scripts/NewClass.cs
+++ Project1/Assets/Scripts/NewClass.cs
@@ -1,27 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
+
+[RequireComponent(typeof(Camera))]
+[RequireComponent(typeof(GameObject))]
+[RequireComponent(typeof(PrimitiveType))]
 
 public class NewClass : MonoBehaviour
 {
-
+    Camera newCamera;
     public UnityEvent uEvent;
     public int scoreValue;
     public string nameString;
     public int intNum;
     public float floatNum;
     public GameObject gameObj;
-    
+    public bool destroyCheck = true;
+
+    public PrimitiveType Sphere;
+
     // Start is called before the first frame update
     private void Start()
     {
         uEvent.Invoke();
+        newCamera = GetComponent<Camera>();
+        Sphere = GetComponent<PrimitiveType>();
+
+        //code from Charger Games tutorial, destroys after 4 seconds
+        //Destroy(gameObject, 4f);
     }
 
+    void DestroyObject()
+    {
+        
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+    }
     // Update is called once per frame
     private void Update()
     {
-        
+        //code from Charger Games Unity youtube video
+        if (Input.GetKeyDown(KeyCode.Space) && destroyCheck)
+        {
+            print("object is gone!");
+            DestroyObject();
+            destroyCheck = false;
+        }
+        // my own adapated version
+        if (Input.GetKeyDown(KeyCode.R) && !destroyCheck)
+        {
+            print("R is pressed");
+            gameObj = GameObject.CreatePrimitive(Sphere);
+            destroyCheck = true;
+            gameObj.tag = "Player";
+        }
     }
 }
