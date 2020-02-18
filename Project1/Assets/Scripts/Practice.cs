aad870f8d2c7fa0cbb64d1e91fd924404ee0103f
Annika <10848892>
2020-02-17 23:42:42 -0700

    Variable practice

---
+++ Project1/Assets/Scripts/Practice.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Practice : MonoBehaviour {
+
+	public string myName = "Annika";
+
+		double age = 10;
+	void Start () {
+
+		age = (float)10;
+
+		var x = 10 + 4;
+
+		Debug.Log(myName + " " + "Schwartz");
+		print(x);
+		print(age);
+
+	}
+	
+
+	void Update () {
+
+		if (myName == "Annika")
+		{
+			print("this is true");
+		}
+		else
+		{
+			print("this isn't true.");
+		}
+	}
+}
