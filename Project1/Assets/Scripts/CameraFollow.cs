3305f3c3edd873f2051c0b15e82a5a25c388667f
Annika <10848892@my.uvu.edu>
2020-02-18 22:37:51 -0700

    Learned Camera Follow and Text UI

---
+++ Project1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // code from Brackeys tutorial on youtube
+    public Transform player;
+    public Vector3 displace;
+
+    void start()
+    {
+        displace = transform.position - player.position;
+    }
+    void Update()
+    {
+        transform.position = player.position + displace;
+    }
+}
