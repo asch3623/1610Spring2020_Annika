c8ea2fdcefae87fc87c6ea6d137d00beac545819
Annika <10848892@my.uvu.edu>
2020-03-03 08:59:35 -0700

    Trail Renderer

---
+++ Project1/Assets/Scripts/FollowCursor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ [RequireComponent(typeof(TrailRenderer))]
+public class FollowCursor : MonoBehaviour
+{
+   
+    // tutorial from https://www.youtube.com/watch?v=_TcEfIXpmRI
+    private TrailRenderer trail;
+    
+    void Start()
+    {
+        trail = GetComponent<TrailRenderer>();
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            trail.startColor = Color.green;
+        }
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = pos;
+    }
+}
