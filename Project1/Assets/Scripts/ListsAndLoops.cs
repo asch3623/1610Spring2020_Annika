8bc8233a581785bcd26ac86984a8e136fcf06ace
Annika <10848892@my.uvu.edu>
2020-03-11 11:13:36 -0600

    Lists and For Loops

---
+++ Project1/Assets/Scripts/ListsAndLoops.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[CreateAssetMenu]
+public class ListsAndLoops : ScriptableObject
+{
+    public List<string> stringList;
+    public string singleString;
+
+    public void AddToList(string stringObj)
+    {
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            if (stringList[i] != stringObj)
+            {
+                stringList.Add(stringObj);
+            }
+        }
+
+    }
+
+    public void SortList()
+    {
+        stringList.Sort();
+    }
+
+    public void RemoveFromList(string stringObj)
+    {
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            if (stringList[i] == stringObj)
+            {
+                stringList.Remove(stringObj);
+            }
+        }
+    }
+    
+public void CheckList()
+    {
+        // looks at each variable in array
+        foreach (var obj in stringList)
+        {
+            if (obj.Contains(singleString))
+            {
+                Debug.Log(obj);
+            }
+        }
+    }
+}
