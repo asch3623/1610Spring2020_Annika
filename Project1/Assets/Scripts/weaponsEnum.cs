673deadfb037a653caf2e304828103423886167c
Annika <10848892@my.uvu.edu>
2020-03-27 22:16:27 -0600

    weapons Switch

---
+++ Project1/Assets/Scripts/weaponsEnum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponsEnum : MonoBehaviour
+{
+    public enum weaponsList
+    {
+     crossbow,
+     sword,
+     knife,
+     bat,
+     laser,
+    }
+
+    public weaponsList currentWeapon;
+void Start()
+{
+    currentWeapon = weaponsList.crossbow;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentWeapon)
+        {
+            case weaponsList.crossbow:
+                break;
+            case weaponsList.sword:
+                break;
+            case weaponsList.knife:
+                break;
+            case weaponsList.bat:
+                break;
+            case weaponsList.laser:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
