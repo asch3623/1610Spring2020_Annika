c023ec36945d59ef01b61ab7fc7bcbca95ea8e04
Annika <10848892@my.uvu.edu>
2020-03-30 11:47:06 -0600

    Sprite Move

---
+++ Project1/Assets/Scripts/SpriteMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMove : MonoBehaviour
+{
+    public float speed = 6f;
+
+    void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+        JumpAction();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
+
+         void JumpAction()
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
+
