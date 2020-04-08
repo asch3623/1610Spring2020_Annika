f1f3e2b71dbe1fe9aca00c2c7077a68d8f2c479a
Annika <10848892@my.uvu.edu>
2020-04-08 11:09:01 -0600

    Flip Player Sprite

---
+++ Project1/Assets/Scripts/AI/PatrolandFollow.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+[RequireComponent(typeof(NavMeshAgent))]
+public class PatrolandFollow : MonoBehaviour
+{
+
+private NavMeshAgent agent;
+public Transform player;
+public float speed = 8f;
+
+private Transform currentDestination;
+
+public List<Transform> patrolPoints;
+private int i;
+private bool canHunt;
+
+void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+        currentDestination = transform;
+    }
+
+private void OnTriggerEnter(Collider other)
+{
+    canHunt = true;
+    currentDestination = player;
+}
+
+private void OnTriggerExit(Collider other)
+{
+    currentDestination = transform;
+    canHunt = false;
+}
+
+void Update()
+{
+    if (canHunt)
+    {
+        agent.destination = currentDestination.position;
+        return;
+    }
+  
+    if (!agent.pathPending && agent.remainingDistance < 0.5f)
+    {
+        agent.destination = patrolPoints[i].position;
+        i = (i + 1) % patrolPoints.Count;
+    }
+}
+
+// follows player all the time
+// void Update(){
+// agent.destination = player.position;
+// }
+}
---
+++ Project1/Assets/Scripts/FlipScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FlipScript : MonoBehaviour
+{
+    public KeyCode key1 = KeyCode.LeftArrow, key2 = KeyCode.RightArrow;
+
+    public float direction1 = 0, direction2 = -180;
+    void Update()
+    {
+        if (Input.GetKeyDown(key1)|| Input.GetKeyDown(KeyCode.A))
+        {
+           transform.rotation = Quaternion.Euler(0,direction2,0);
+        } 
+        if (Input.GetKeyDown(key2)||Input.GetKeyDown(KeyCode.D))
+        {
+             transform.rotation = Quaternion.Euler(0,direction1,0);
+        } 
+    }
+
+    public void Flip(float value)
+    {
+        transform.Rotate (0,value,0);
+    }
+}
