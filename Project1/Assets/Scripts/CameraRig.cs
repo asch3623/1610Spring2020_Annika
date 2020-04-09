e6aa15894e40303403c0d465bfe0da44f5f1ddc0
Annika <10848892@my.uvu.edu>
2020-04-08 18:38:02 -0600

    followed tutorial, camera and move

---
+++ Project1/Assets/Scripts/CameraRig.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour
+{
+    // code from https://www.raywenderlich.com/980-introduction-to-unity-scripting
+    public float moveSpeed;
+    public GameObject target;
+
+    private Transform rigTransform;
+    void Start()
+    {
+        rigTransform = this.transform.parent;
+    }
+
+    
+    void FixedUpdate()
+    {
+        if (target = null)
+        {
+            return;
+        }
+
+        rigTransform.position = Vector3.Lerp(rigTransform.position,
+            target.transform.position, Time.deltaTime * moveSpeed);
+    }
+    
+}
---
+++ Project1/Assets/Scripts/Move/SphereMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class SphereMove : MonoBehaviour
+{
+   // code from https://www.raywenderlich.com/980-introduction-to-unity-scripting
+   public float acceleration;
+   public float maxSpeed;
+
+   private Rigidbody rigidBody;
+   private KeyCode[] inputKeys;
+   private Vector3[] directionsForKeys;
+
+   void Start()
+   {
+       inputKeys = new KeyCode[] {KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D};
+       directionsForKeys = new Vector3[] {Vector3.forward, Vector3.left, Vector3.back, Vector3.right};
+       rigidBody = GetComponent<Rigidbody>();
+   }
+
+    // use fixedupdate with rigidbodies
+    void FixedUpdate()
+    {
+        for (int i = 0; i < inputKeys.Length; i++)
+        {
+            var key = inputKeys[i];
+            // checks for input keys
+            if (Input.GetKey(key))
+            {
+                //moves player in direction of input key
+                Vector3 movement = directionsForKeys[i] * acceleration * Time.deltaTime;
+                movePlayer(movement);
+            }
+        }
+    }
+//limits max speed
+    void movePlayer(Vector3 movement)
+    {
+        if (rigidBody.velocity.magnitude * acceleration > maxSpeed)
+        {
+            rigidBody.AddForce(movement * -1);
+        }
+        else
+        {
+            rigidBody.AddForce(movement);
+        }
+    }
+}
