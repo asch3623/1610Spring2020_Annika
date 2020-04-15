619a1a4d24314742cfc34a7c636b7919e7161b8d
Annika <10848892@my.uvu.edu>
2020-04-14 23:20:39 -0600

    Shoot

---
+++ Project1/Assets/Scripts/ShooterScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterScript : MonoBehaviour
+{
+    //tutorial from https://www.youtube.com/watch?v=MoaFVfb9ksw
+
+    public Transform player;
+    public float speed = 5f;
+    public GameObject bullet;
+    
+    void Update()
+    {
+        moveCharacter(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+        if (Input.GetKeyDown("space"))
+        {
+            shootBullet();
+        }
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        player.Translate(direction*speed*Time.deltaTime);
+    }
+
+    public void shootBullet()
+    {
+        GameObject laser = Instantiate(bulletPreFab) as GameObject;
+        laser.transform.position = player.transform.position;
+    }
+}
