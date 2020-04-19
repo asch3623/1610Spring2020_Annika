52cbb04a2cac37ee2a6f35dfd375df6422bcf6b1
Annika <10848892@my.uvu.edu>
2020-04-18 22:58:37 -0600

    IfStatements Review

---
+++ Project1/Assets/Scripts/ElseStatementReview.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ElseStatementReview : MonoBehaviour
+{
+   
+   int startingInt = 1;
+   int b = 3;
+   int c = 1;
+   int d = 4;
+   
+    void Start()
+    {
+   if (b + c == d)
+   {
+       Debug.Log("This is correct.");
+   }     
+   
+   else if (startingInt != 0)
+   {
+       Debug.Log("Starting Int is not 0");
+   }
+    }
+}
