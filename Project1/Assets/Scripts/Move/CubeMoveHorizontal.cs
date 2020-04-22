c57292e952ec493b0d18a1c9abeb70564e55afdd
Annika <10848892@my.uvu.edu>
2020-04-21 20:57:15 -0600

    GravitySwitch

---
+++ Project1/Assets/Scripts/Move/CubeMoveHorizontal.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class CubeMoveHorizontal : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 positionDirection;
+    public float speed = 6f;
+    public float jumpForce = 30f;
+    public int jumpCountMax = 2;
+    public int jumpCount;
+
+    public UnityEvent jumpEvent;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        controller.Move(positionDirection * Time.deltaTime);
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+       
+
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            positionDirection.y = 0;
+        }
+        
+        //double jump
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
+        {
+            jumpEvent.Invoke();
+            positionDirection.y = jumpForce;
+            jumpCount++;
+        }
+        
+        //gravity
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("going Up");
+            Physics.gravity = new Vector3(0f,9.81f,0f);
+            jumpForce = -30f;
+
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("going down");
+            Physics.gravity = new Vector3(0f,-9.81f,0f);
+
+            jumpForce = 30f;
+        }
+        
+        
+    }
+}
---
+++ Project1/Assets/Scripts/Move/GravityController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class GravityController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    private Vector2 moveVelocity;
+    public int collisions = 0;
+
+void OnCollisionEnter(Collision collision)
+{
+    if (collision.collider.tag == "Ground")
+    {
+        collisions++;
+    }
+  
+}
+
+void OnCollisionExit(Collision collision)
+
+{
+    if (collision.collider.tag == "Ground")
+    {
+        collisions--;
+    }
+}
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+
+        moveVelocity = moveInput.normalized * speed;
+    }
+    private void FixedUpdate()
+    {
+        
+    rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+
+   if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && collisions > 0)
+        {
+            rb.gravityScale = -100;
+        }  
+        if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && collisions == 0)
+        {
+            rb.gravityScale = 100;
+        }  
+    }
+}
