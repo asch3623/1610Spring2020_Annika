bf4c4c9121e82a12fcac1b8194a11fa6f3cee3ca
Annika <10848892@my.uvu.edu>
2020-04-26 22:40:04 -0600

    Moved Scenes, Start Menu

---
+++ Project1/Assets/Scripts/MainMenuClose.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
+
+public class MainMenuClose : MonoBehaviour
+{
+
+    public Image menu;
+    public Text starFinder;
+    
+    private void OnMouseUp()
+    {
+        enabled = false;
+        starFinder.enabled = false;
+        menu.enabled = false;
+    }
+    
+}
